e520e095eb3088faeeff53383fe05b5e1f695ce2
Yusuf Serdar Yazıcıoğlu <frekons@gmail.com>
2020-12-24 21:40:12 +0300

    Update MemoryManager.h

--- MemoryManager.h
+++ MemoryManager.h
@@ -6,6 +6,8 @@
 
 #include "Module.h"
 
+#define throws_exception
+
 class memory_manager
 {
 protected:
@@ -32,51 +34,69 @@ public:
 
 	bool read(uintptr_t address, void* buffer, size_t size);
 
-	template <class T>
-	T read(uintptr_t address)
-	{
-		T value;
+	template <class T> T read(uintptr_t address);
 
-		ReadProcessMemory(this->handle, (LPCVOID)address, &value, sizeof(T), 0);
-
-		return value;
-	}
-
-	template <class T>
-	bool write(uintptr_t address, T value)
-	{
-		size_t numbers_of_bytes_written = 0;
-
-		WriteProcessMemory(this->handle, (LPCVOID)address, &value, sizeof(T), &numbers_of_bytes_written);
-
-		return numbers_of_bytes_written == sizeof(T);
-	}
+	template <class T> bool write(uintptr_t address, T value);
 
 	// operators
 
 	inline module_t operator()(std::string module_name);
 
-	template<class T>
-	inline T operator()(uintptr_t address) 
-	{
-		return read<T>(address);
-	}
+	template<class T> inline T operator()(uintptr_t address);
+	
+	/*
+	 "type" parameter because, i could not find a way to call a operator with template
+	 however, if you want to read non-primitive types you should call it like manager.read<T>(address) because in this function there is additional T parameter and
+	 it will copy all the bytes of type T to call this function, that's like nothing in primitive types but not in non-primitive types
+	*/
+	template<class T> T operator()(std::string module_name, uintptr_t address, T type) throws_exception;
 
-	template<class T>
-	T operator()(std::string module_name, uintptr_t address, T type)
-	{
-		auto module = get_module(module_name);
 
-		if (!module.is_valid())
-			throw InvalidModule();
-
-		return read<T>(module.base_address + address);
-	}
 
 	// exceptions
+
 	class InvalidModule : std::exception
 	{
 	public:
 		std::exception::exception;
 	};
 };
+
+
+
+template<class T>
+inline T memory_manager::read(uintptr_t address)
+{
+	T value;
+
+	ReadProcessMemory(this->handle, (LPCVOID)address, &value, sizeof(T), 0);
+
+	return value;
+}
+
+template<class T>
+inline bool memory_manager::write(uintptr_t address, T value)
+{
+	size_t numbers_of_bytes_written = 0;
+
+	WriteProcessMemory(this->handle, (LPCVOID)address, &value, sizeof(T), &numbers_of_bytes_written);
+
+	return numbers_of_bytes_written == sizeof(T);
+}
+
+template<class T>
+inline T memory_manager::operator()(uintptr_t address)
+{
+	return read<T>(address);
+}
+
+template<class T>
+inline T memory_manager::operator()(std::string module_name, uintptr_t address, T type)
+{
+	auto module = get_module(module_name);
+
+	if (!module.is_valid())
+		throw InvalidModule();
+
+	return read<T>(module.base_address + address);
+}
