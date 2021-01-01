0f86e36ed177c6fd6d345834d16f2360acd15b88
Yusuf Serdar Yazıcıoğlu <frekons@gmail.com>
2021-01-01 09:28:16 +0300

    Update test-main.cpp

--- test-main.cpp
+++ test-main.cpp
@@ -6,9 +6,9 @@ int main() {
 
 	const char* app_name = "brave.exe", *module_name = "brave.exe";
 
-	extern_memory manager(app_name);
+	extern_memory memory(app_name);
 
-	if (!manager.is_attached())
+	if (!memory.is_attached())
 	{
 		std::cout << "failed to attach! error: 0x" << std::hex << GetLastError() << std::dec << std::endl;
 		return 0;
@@ -16,7 +16,7 @@ int main() {
 
 	std::cout << "attached to \"" << app_name << "\" successfully, MZ test: \"";
 
-	auto mz_text = manager(module_name, 0, (short)0);
+	auto mz_text = memory(module_name, 0, (short)0);
 
 	std::cout << ((char*)(&mz_text))[0] << ((char*)(&mz_text))[1] << "\"" << std::endl;
 
